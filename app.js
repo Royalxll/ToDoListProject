cd40139c716ce322a85fefaf3dfbba1dc15a0c62
Royalxll <wrightkaseem24@gmail.com>
2021-10-04 15:15:10 -0400

    Add gitignore, and update ports

--- app.js
+++ app.js
@@ -126,15 +126,17 @@ app.post("/delete", function(req, res){
       }
     });
   }
-
-
-
 });
 
 app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {   //process.env.PORT replaces port 3000
+  console.log("Server started on port Successfully");
 });
